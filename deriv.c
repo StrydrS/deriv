c34848f61fea7f67fb1bdfae3dcf6ebde32f3bd2
StrydrS <strydr8@gmail.com>
2024-10-22 07:23:33 -0600

    completed basic trig functions

--- deriv.c
+++ deriv.c
@@ -7,6 +7,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+//returns 1 if function starts with a - 
+int isNegative(char *startFunction) { 
+  int flag = 0;
+  if(startFunction[0] == '-') { 
+    flag = 1; 
+    return flag;
+  } return flag;
+}
+
 // reverses strings
 char *strrev(char *str) {
   char *p1, *p2;
@@ -71,22 +81,22 @@ int parsePower(char *startFunction) {
 }
 
 int isFunctionOfX(char *startFunction) {
-  
-  int flag = 0; 
+
+  int flag = 0;
   int len = strlen(startFunction);
-  for(int i = 0; i < len; i++) { 
-    if(startFunction[i] == 'x') { 
-      flag = 1; 
+  for (int i = 0; i < len; i++) {
+    if (startFunction[i] == 'x') {
+      flag = 1;
       return flag;
     }
-  } return flag; 
-
+  }
+  return flag;
 }
 // uses parsePower and parseBase to create a calculate power rule for given
 // function
 
 char *powerRule(char *startFunction) {
-  
+
   int len = strlen(startFunction);
   int base, power, flag;
   char *result = (char *)malloc(sizeof(char) * len);
@@ -98,18 +108,99 @@ char *powerRule(char *startFunction) {
   if ((power == 0) & (flag == 1)) {
     sprintf(result, "%d", base);
     return result;
-  } else if((power == 0) & (flag == 0)) { 
+  } else if ((power == 0) & (flag == 0)) {
     return NULL;
-  } else if((power != 0) & (flag == 1)) { 
+  } else if ((power != 0) & (flag == 1)) {
     base = base * power;
     power = power - 1;
-    if(power == 1) {
+    if (power == 1) {
       sprintf(result, "%dx", base);
       return result;
-    } else { 
+    } else {
       sprintf(result, "%dx^%d", base, power);
       return result;
     }
+  }
+  return NULL;
+}
+
+// function calculates trig derivatives, assumes given function is a trig function
+char *trigFunction(char *startFunction) { 
+  
+  int len = strlen(startFunction);
+  int flag = isNegative(startFunction);
+  char *result = (char *)malloc(sizeof(char) * len);
+
+  //iterates pointer to remove first character
+  if(flag == 1) { 
+    startFunction++;
+  }
+  
+  switch(startFunction[0]) { 
+    case 'c':
+      if(startFunction[2] == 's') {
+        if(flag == 1) { 
+          strcpy(result, "sin(x)");
+          return result;
+          break;
+        } else { 
+          strcpy(result, "-sin(x)");
+          return result;
+          break; 
+        }
+      } else if(startFunction[2] == 't') { 
+          if(flag == 1) { 
+            strcpy(result, "csc^2(x)");
+            return result;
+            break;
+          } else { 
+            strcpy(result, "-csc^2(x)");
+            return result;
+            break; 
+        }
+      } else { 
+          if(flag == 1) { 
+            strcpy(result, "csc(x)cot(x)");
+            return result;
+            break;
+          } else { 
+            strcpy(result, "-csc(x)cot(x)");
+            return result;
+            break; 
+          }
+      }
+    case 's':
+      if(startFunction[1] == 'i') { 
+        if(flag == 1) { 
+          strcpy(result, "-cos(x)");
+          return result;
+          break;
+        } else { 
+          strcpy(result, "cos(x)");
+          return result;
+          break;
+        } 
+      } else {     
+        if(flag == 1) { 
+          strcpy(result, "-sec(x)tan(x)");
+          return result;
+          break;
+        } else { 
+          strcpy(result, "sec(x)tan(x)");
+          return result;
+          break;
+        }
+      }
+    case 't':
+      if(flag == 1) { 
+        strcpy(result, "-sec^2(x)");
+        return result;
+        break;
+      } else { 
+        strcpy(result, "sec^2(x)");
+        return result;
+        break;
+      } 
   }
   return NULL;
 }
@@ -121,12 +212,15 @@ int main() {
   char *testFunction2 = "5x^2";
   char *testFunction5 = "7012x^2013";
   char *testFunction4 = "909090900x^2";
+  char *testFunction1 = "-csc(x)";
   
-  char* result = powerRule(testFunction);
+  printf("Derivative of %s is %s\n", testFunction1 ,trigFunction(testFunction1));
 
-  if(result == NULL) { 
-    printf("Derivative of %s is 0\n", testFunction); 
-  } else { 
+  char *result = powerRule(testFunction);
+
+  if (result == NULL) {
+    printf("Derivative of %s is 0\n", testFunction);
+  } else {
     printf("Derivative of %s is %s\n", testFunction, result);
   }
   powerRule(testFunction3);
