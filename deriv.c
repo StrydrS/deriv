dc203aabc2763f49a5540cc43464c22e40a62617
StrydrS <strydr8@gmail.com>
2024-10-22 19:00:00 -0600

    broken af but ok

--- deriv.c
+++ deriv.c
@@ -7,32 +7,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-char *parseFunction(char *startFunction) { 
+char *parseFunction(char *startFunction) {
   printf("%s", startFunction);
   int len = strlen(startFunction);
   char *result = (char *)malloc(sizeof(char) * len);
- 
-  for(int i = 0; i < len - 1; i++) {
-    if(startFunction[i] == '(') { 
-        strcat(result, &startFunction[i]);
-      }
+
+  for (int i = 0; i < len - 1; i++) {
+    if (startFunction[i] == '(') {
+      strcat(result, &startFunction[i]);
     }
-  
+  }
+
   result++;
   len = strlen(result);
 
   result[len - 1] = '\0';
   return result;
-
 }
-//returns 1 if function starts with a - 
-int isNegative(char *startFunction) { 
+// returns 1 if function starts with a -
+int isNegative(char *startFunction) {
   int flag = 0;
-  if(startFunction[0] == '-') { 
-    flag = 1; 
+  if (startFunction[0] == '-') {
+    flag = 1;
     return flag;
-  } return flag;
+  }
+  return flag;
 }
 
 // reverses strings
@@ -67,51 +66,52 @@ int parseBase(char *startFunction) {
   }
   return ret;
 }
-char* floatToFrac(float f, int divisor) { 
+char *floatToFrac(float f, int divisor) {
   int dividend = 0;
   int completeFlag = 0;
-  float test;
-  
-  char *frac = (char *)malloc(sizeof(char) * 64);;
-  
+  double test;
+
+  char *frac = (char *)malloc(sizeof(char) * 64);
+
   printf("Divisor: %d\n", divisor);
-  while(completeFlag != 1) { 
-    if(f < 0) { 
+  while (completeFlag != 1) {
+    if ((double)f <= 1.0) {
       dividend -= 1;
-      test = (float)dividend/(float)divisor;
-      if(test == f) { 
+      test = (double)dividend / (double)divisor;
+      printf("test: %f\n", test);
+      if (test == (double)f) {
         completeFlag = 1;
       }
-    } else if(f >= 0) { 
+    } else if((double)f > 1.0) {
       dividend += 1;
-      test = (float)dividend/(float)divisor;
-      if(test == f) { 
+      test = (double)dividend / (double)divisor;
+      printf("test: %f\n", test);
+      if (test == (double)f) {
         completeFlag = 1;
-      } 
+      }
     }
   }
-  sprintf(frac, "%d/%d", dividend, divisor); 
+  sprintf(frac, "%d/%d", dividend, divisor);
   printf("%s\n", frac);
   return frac;
 }
 
-char* parsePowerFloat(char *insideFunction) {
-    int len = strlen(insideFunction);  
-    char *tempDividend = (char *)malloc(sizeof(char) * len);
-    char *tempDivisor = (char *)malloc(sizeof(char) * len);
-    int divisor, dividend; 
-    int divFlag = 1;
-    insideFunction = strrev(insideFunction);
-    for(int q = 0; q < len + 1; q++) { 
-      if((insideFunction[q] != '/') & (divFlag == 1)) { 
-        strncat(tempDividend, &insideFunction[q], 1);
-      } else if(insideFunction[q] == '/') {
-        divFlag = 0; 
-      } else
-      if((insideFunction[q] != '/') & (divFlag == 0)) { 
-        strncat(tempDivisor, &insideFunction[q], 1);
-      }
+char *parsePowerFloat(char *insideFunction) {
+  int len = strlen(insideFunction);
+  char *tempDividend = (char *)malloc(sizeof(char) * len);
+  char *tempDivisor = (char *)malloc(sizeof(char) * len);
+  int divisor, dividend;
+  int divFlag = 1;
+  insideFunction = strrev(insideFunction);
+  for (int q = 0; q < len + 1; q++) {
+    if ((insideFunction[q] != '/') & (divFlag == 1)) {
+      strncat(tempDividend, &insideFunction[q], 1);
+    } else if (insideFunction[q] == '/') {
+      divFlag = 0;
+    } else if ((insideFunction[q] != '/') & (divFlag == 0)) {
+      strncat(tempDivisor, &insideFunction[q], 1);
     }
+  }
 
   divisor = atoi(tempDivisor);
   dividend = atoi(tempDividend);
@@ -121,10 +121,10 @@ char* parsePowerFloat(char *insideFunction) {
   printf("frac before: %f\n", frac);
   frac = frac - 1.0;
   printf("frac after: %f\n", frac);
-  
+
   char *ret = floatToFrac(frac, divisor);
-  return NULL;  
-  }
+  return NULL;
+}
 // parses dstartFunction for the power
 int parsePower(char *startFunction) {
 
@@ -144,23 +144,23 @@ int parsePower(char *startFunction) {
 
   int isFraction = 0;
   int len = strlen(result);
-  
-  for(int z = 0; z < len; z++) { 
-    if(result[z] == '/') { 
+
+  for (int z = 0; z < len; z++) {
+    if (result[z] == '/') {
       isFraction = 1;
       parsePowerFloat(result);
       break;
     }
   }
 
-  if((len <= 1) & (isFraction == 0)) {
+  if ((len <= 1) & (isFraction == 0)) {
     ret = atoi(result);
     free(result);
     return ret;
   }
 
   result = strrev(result);
-  
+
   return ret;
 }
 
@@ -188,9 +188,9 @@ char *powerRule(char *startFunction) {
   power = parsePower(startFunction);
   base = parseBase(startFunction);
   flag = isFunctionOfX(startFunction);
-  
+
   if ((power == 0) & (flag == 1)) {
-    if(base == 0) { 
+    if (base == 0) {
       base = 1;
     }
     sprintf(result, "%d", base);
@@ -198,11 +198,11 @@ char *powerRule(char *startFunction) {
   } else if ((power == 0) & (flag == 0)) {
     return NULL;
   } else if ((power != 0) & (flag == 1)) {
-    if(base == 0) { 
+    if (base == 0) {
       base = 1;
-    } else if(power == 0) { 
+    } else if (power == 0) {
       power = 1;
-    } else if(power == 0 & base == 0) {
+    } else if (power == 0 & base == 0) {
       base = 1;
       power = 1;
     }
@@ -212,7 +212,7 @@ char *powerRule(char *startFunction) {
     if (power == 1) {
       sprintf(result, "%dx", base);
       return result;
-    } else if(power == 0) {
+    } else if (power == 0) {
       sprintf(result, "%d", base);
       return result;
     } else {
@@ -223,108 +223,107 @@ char *powerRule(char *startFunction) {
   return NULL;
 }
 
-// function calculates trig derivatives, assumes given function is a trig function
-char *trigFunction(char *startFunction) { 
-  
+// function calculates trig derivatives, assumes given function is a trig
+// function
+char *trigFunction(char *startFunction) {
+
   int len = strlen(startFunction);
   int signFlag = isNegative(startFunction);
   int xFlag = 0;
   char *result = (char *)malloc(sizeof(char) * len);
   char *insideFunction = parseFunction(startFunction);
-  if(strcmp(insideFunction, "x") == 1) { 
-     
+  if (strcmp(insideFunction, "x") == 1) {
   }
-  //iterates pointer to remove first character
-  if(signFlag == 1) { 
+  // iterates pointer to remove first character
+  if (signFlag == 1) {
     startFunction++;
   }
-  switch(startFunction[0]) { 
-    case 'c':
-       
-      if(startFunction[2] == 's') {
-        if(signFlag == 1) { 
-          sprintf(result, "sin(%s)", insideFunction);
-          break;
-        } else { 
-          sprintf(result, "-sin(%s)", insideFunction); 
-          break; 
-        }
-      } else if(startFunction[2] == 't') { 
-          if(signFlag == 1) { 
-            sprintf(result, "csc^2(%s)", insideFunction); 
-            break;
-          } else {  
-            sprintf(result, "-csc^2(%s)", insideFunction); 
-            break; 
-        }
-      } else { 
-          if(signFlag == 1) { 
-            sprintf(result, "csc(%s)cot(%s)", insideFunction, insideFunction); 
-            break;
-          } else { 
-            sprintf(result, "-csc(%s)cot(%s)", insideFunction, insideFunction); 
-            break; 
-          }
-      }
-    case 's':
-      if(startFunction[1] == 'i') { 
-        if(signFlag == 1) {  
-          sprintf(result, "-cos(%s)", insideFunction); 
-          break;
-        } else {  
-          sprintf(result, "cos(%s)", insideFunction); 
-          break;
-        } 
-      } else {     
-        if(signFlag == 1) {  
-          sprintf(result, "-sec(%s)tan(%s)", insideFunction, insideFunction); 
-          break;
-        } else {  
-          sprintf(result, "sec(%s)tan(%s)", insideFunction, insideFunction); 
-          break;
-        }
-      }
-    case 't':
-      if(signFlag == 1) {  
-        sprintf(result, "sec^2(%s)", insideFunction); 
+  switch (startFunction[0]) {
+  case 'c':
+
+    if (startFunction[2] == 's') {
+      if (signFlag == 1) {
+        sprintf(result, "sin(%s)", insideFunction);
         break;
-      } else {  
-        sprintf(result, "-sec^2(%s)", insideFunction); 
+      } else {
+        sprintf(result, "-sin(%s)", insideFunction);
         break;
-      } 
+      }
+    } else if (startFunction[2] == 't') {
+      if (signFlag == 1) {
+        sprintf(result, "csc^2(%s)", insideFunction);
+        break;
+      } else {
+        sprintf(result, "-csc^2(%s)", insideFunction);
+        break;
+      }
+    } else {
+      if (signFlag == 1) {
+        sprintf(result, "csc(%s)cot(%s)", insideFunction, insideFunction);
+        break;
+      } else {
+        sprintf(result, "-csc(%s)cot(%s)", insideFunction, insideFunction);
+        break;
+      }
+    }
+  case 's':
+    if (startFunction[1] == 'i') {
+      if (signFlag == 1) {
+        sprintf(result, "-cos(%s)", insideFunction);
+        break;
+      } else {
+        sprintf(result, "cos(%s)", insideFunction);
+        break;
+      }
+    } else {
+      if (signFlag == 1) {
+        sprintf(result, "-sec(%s)tan(%s)", insideFunction, insideFunction);
+        break;
+      } else {
+        sprintf(result, "sec(%s)tan(%s)", insideFunction, insideFunction);
+        break;
+      }
+    }
+  case 't':
+    if (signFlag == 1) {
+      sprintf(result, "sec^2(%s)", insideFunction);
+      break;
+    } else {
+      sprintf(result, "-sec^2(%s)", insideFunction);
+      break;
+    }
   }
-  if(strcmp(insideFunction, "x\0") == 1) { 
+  if (strcmp(insideFunction, "x\0") == 1) {
     return result;
   } else {
-    if(result[0] == '-') {
+    if (result[0] == '-') {
       signFlag = 1;
     } else {
       signFlag = 0;
     }
     char *insideDeriv = powerRule(insideFunction);
     char *temp = result;
-    if(signFlag == 1) { 
+    if (signFlag == 1) {
       result++;
       strcat(insideDeriv, result);
       memmove(insideDeriv + 1, insideDeriv, strlen(insideDeriv) + 1);
       insideDeriv[0] = '-';
-    } else { 
+    } else {
       strcat(insideDeriv, result);
     }
     return insideDeriv;
-  } 
+  }
   return result;
 }
-
 
 int main() {
   char testFunction[256];
 
   // runs prompt until user enters quit
-  while(strcmp(testFunction, "quit")) {  
+  while (strcmp(testFunction, "quit")) {
     printf("Function to differentiate: ");
-    scanf("%s", testFunction);   
-    if(!strcmp(testFunction, "quit\0")) { 
+    scanf("%s", testFunction);
+    if (!strcmp(testFunction, "quit\0")) {
       return 0;
     }
     printf("Trig Derivative: %s\n", trigFunction(testFunction));
